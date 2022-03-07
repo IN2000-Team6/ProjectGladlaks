e7aa818f4479941018780a9b3cde2b1077783846
martinmjensen <mamjense@ulrik.uio.no>
2022-03-07 11:27:38 +0100

    Update MainActivity.kt

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/MainActivity.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Velkommen til $name!")
 }
 
 @Preview(showBackground = true)
