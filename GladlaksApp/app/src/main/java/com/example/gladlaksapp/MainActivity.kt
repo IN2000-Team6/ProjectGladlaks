f651be71b9557b3a4d9288d6d05e1e987c056cd0
martinmjensen <mamjense@ulrik.uio.no>
2022-03-09 19:53:48 +0100

    Added updated styling to test branch

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/MainActivity.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.gladlaksapp
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -23,9 +20,11 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
-
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,14 +35,46 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column {
-                        Greeting(resources.getString(R.string.app_name))
-                        Card(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .height(350.dp)
-                                .shadow(8.dp)
+                        val scope = rememberCoroutineScope()
+
+                        val sheetState = rememberBottomSheetScaffoldState(
+                            bottomSheetState = rememberBottomSheetState(
+                                initialValue = BottomSheetValue.Collapsed
+                            )
+                        )
+
+                        BottomSheetScaffold(
+                            sheetContent = {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier
+                                        .padding(bottom = 30.dp, start = 16.dp, end = 16.dp, top = 16.dp)
+                                ) {
+                                    Text(
+                                        text = "Detaljer",
+                                        modifier = Modifier
+                                            .padding(bottom = 4.dp)
+                                    )
+                                    CustomBarChart(400)
+                                }
+                            },
+                            scaffoldState = sheetState,
+                            sheetElevation = 20.dp,
+                            sheetPeekHeight = 65.dp,
                         ) {
-                            ChartTest(10)
+                            Column(
+                                modifier = Modifier.padding(20.dp)
+                            ) {
+                                Greeting(resources.getString(R.string.app_name))
+                                Button(
+                                    modifier = Modifier.padding(top = 10.dp),
+                                    onClick = {
+                                        scope.launch { sheetState.bottomSheetState.expand()
+                                    }
+                                }) {
+                                    Text("VIS GRAF")
+                                }
+                            }
                         }
                     }
                 }
@@ -53,22 +84,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ChartTest(padding: Int) {
+fun CustomBarChart(height: Int) {
     AndroidView(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(padding.dp),
+            .fillMaxWidth()
+            .height(height.dp),
         factory = { context ->
             BarChart(context).apply {
                 val data = BarData(getDate())
                 data.barWidth = 0.2f
                 this.data = data
-                this.animateY(500);
-                this.invalidate();
+                this.animateY(500)
+                this.invalidate()
                 this.xAxis.position = XAxis.XAxisPosition.BOTTOM
                 this.axisRight.isEnabled = false
                 this.description.isEnabled = false
-                this.setPinchZoom(false)
+                this.setScaleEnabled(false)
+                this.isHovered = false
+                this.axisLeft.axisMinimum = 0f
             }
         },
     )
@@ -76,31 +109,24 @@ fun ChartTest(padding: Int) {
 
 @Composable
 fun Greeting(name: String) {
-    Text("Velkommen til $name!!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GladlaksAppTheme {
-        Greeting("Hello")
-    }
+    Text("Velkommen til $name!")
 }
 
 fun getDate(): List<IBarDataSet> {
     val set1 = mutableListOf(
-        BarEntry(0f,110.000f),
-        BarEntry(1f,40.000f),
-        BarEntry(2f,60.000f),
-        BarEntry(3f,30.000f),
-        BarEntry(4f,90.000f),
-        BarEntry(5f,100.000f),
-        BarEntry(6f,150.000f),
+        BarEntry(0f, 110.000f),
+        BarEntry(1f, 40.000f),
+        BarEntry(2f, 60.000f),
+        BarEntry(3f, 30.000f),
+        BarEntry(4f, 90.000f),
+        BarEntry(5f, 100.000f),
+        BarEntry(6f, 150.000f),
+        BarEntry(7f, 150.000f),
     )
 
     val barDataSet1 = BarDataSet(set1, "Data label")
 
-    barDataSet1.setColors(*ColorTemplate.COLORFUL_COLORS)
+    barDataSet1.setColors(*ColorTemplate.PASTEL_COLORS)
 
     return listOf(barDataSet1)
 }
