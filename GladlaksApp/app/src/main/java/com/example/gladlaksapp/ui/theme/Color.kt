5d0e6f98e4a84aa4ae6bf8e0dd5f1006a8af9986
martinmjensen <mamjense@ulrik.uio.no>
2022-03-10 13:28:42 +0100

    Revert "Merge pull request #8 from team6/chartTest"
    This reverts commit f0ad7c553ca0fad7ad9aff572de8691a994baa88, reversing
    changes made to 20aeb037ce351109c7fc73495817bbae9cbb5f92.

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Color.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.gladlaksapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Gray200 = Color(0xFFCCCCCC)
-val Gray500 = Color(0xFF333333)
-val Gray700 = Color(0xFF111111)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Theme.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Theme.kt
@@ -7,14 +7,14 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Gray200,
-    primaryVariant = Gray700,
+    primary = Purple200,
+    primaryVariant = Purple700,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Gray500,
-    primaryVariant = Gray700,
+    primary = Purple500,
+    primaryVariant = Purple700,
     secondary = Teal200
 
     /* Other default colors to override
