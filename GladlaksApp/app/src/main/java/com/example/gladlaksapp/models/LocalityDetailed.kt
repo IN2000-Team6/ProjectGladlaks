03c1ec106dc2e5abfe87d5919df92664b7ef81fa
Jesper Dahl Norgård <jesperdn@ulrik.uio.no>
2022-03-20 18:41:39 +0100

    Added treatment info

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/models/LocalityDetailed.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/models/LocalityDetailed.kt
@@ -1,11 +1,32 @@
 package com.example.gladlaksapp.models
 
+// ------------- Class Structure ------------- //
+// LocalityDetailed {
+//      LocalityWeek {
+//              BathTreatments [
+//                  Treatment {
+//                      TreatmentDetail {
+//                          Substance {
+//                          }
+//                      }
+//                  }
+//              ]
+//              (Duplicate for infeed-treatments)
+//              MechanicalRemoval
+//      }
+// }
+
+
+// ------------- Outer Wrapper Class ------------- //
+
 data class LocalityDetailed (
     val localityName: String,
     val localityWeek: LocalityWeek,
     val liceCountPreviousWeek: LiceCountPreviousWeek,
 
 )
+
+// ------------- Detailed Info ------------- //
 
 data class LocalityWeek (
     val id: Int,
@@ -20,7 +41,13 @@ data class LocalityWeek (
     val avgAdultFemaleLice: Number,
     val avgMobileLice: Number,
     val avgStationaryLice: Number,
+    val bathTreatments: List<Treatment>?,
+    val inFeedTreatments: List<Treatment>?,
+    val mechanicalRemoval: MechanicalRemoval?,
 )
+
+
+// ------------- Previous Week Lice Count ------------- //
 
 data class LiceCountPreviousWeek (
     val year: Int,
@@ -31,3 +58,40 @@ data class LiceCountPreviousWeek (
     val avgStationaryLice: Number
 
 )
+
+
+// ------------- Mechanical Removal ------------- //
+
+data class MechanicalRemoval(
+    val id: Int,
+    val entireLocality: Boolean,
+    val method: String,
+)
+
+
+// ------------- In-feed Or Bath Treatments ------------- //
+
+data class Treatment(
+    val id: Int,
+    val treatmentType: String,
+    val startDate: String,
+    val endDate: String,
+    val entireLocality: Boolean,
+    val treatmentDetail: List<TreatmentDetail>
+)
+
+data class TreatmentDetail(
+    val id: Int,
+    val quantity: Int,
+    val concentration: Number,
+    val substanceId: Int,
+    val substance: Substance,
+)
+
+data class Substance (
+    val id: Int,
+    val name: String
+)
+
+
+// ------------- Cleaner Fish ------------- //
