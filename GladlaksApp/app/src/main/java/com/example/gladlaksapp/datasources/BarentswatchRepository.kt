07f00951dd8809645cb976e43897fba28a820ac8
tonehilmen <tone.eide.hilmen@gmail.com>
2022-03-22 15:26:47 +0100

    Add test for BarentswatchRepository

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/datasources/BarentswatchRepository.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/datasources/BarentswatchRepository.kt
@@ -3,15 +3,16 @@ package com.example.gladlaksapp.datasources
 import com.example.gladlaksapp.models.LocalitiesWrapper
 import com.example.gladlaksapp.models.LocalityDetailsWrapper
 
-val datasource = BarentswatchNetworkDataSource
+object BarentswatchRepository {
+    val datasource = BarentswatchNetworkDataSource
+
+    suspend fun getLocalities(year: Int, week: Int) : LocalitiesWrapper {
+        return datasource.getLocalities(year,week)
+    }
 
 
-
-suspend fun getLocalities(year: Int, week: Int) : LocalitiesWrapper {
-    return datasource.getLocalities(year,week)
+    suspend fun getDetailedLocalityInfo(localityNo: Int, year: Int, week: Int) : LocalityDetailsWrapper {
+        return datasource.getDetailedLocalityInfo(localityNo, year, week)
+    }
 }
 
-
-suspend fun getDetailedLocalityInfo(localityNo: Int, year: Int, week: Int) : LocalityDetailsWrapper {
-    return datasource.getDetailedLocalityInfo(localityNo, year, week)
-}
