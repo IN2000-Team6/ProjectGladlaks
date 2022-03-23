ed34c3bc9fc6cc238f621daffb358c70bbf29c00
ErikMarketer <erikoa@ulrik.uio.no>
2022-03-23 14:00:03 +0100

    Expand/collapse bottomsheet buttons

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/bottomSheet/BottomSheet.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/bottomSheet/BottomSheet.kt
@@ -1,32 +1,89 @@
 package com.example.gladlaksapp.composables.bottomSheet
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@Preview
+@Composable
+fun MapsScreen() {
+    BottomSheetLayout(30)
+}
+
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun mapsView(){
+fun BottomSheetLayout(
+    selectedPeekHeight: Int,
+    //content: @Composable () -> Unit,
+    //sheetContent: @Composable () -> Unit
+) {
+    val initialPeekHeight = 0
+
     val sheetState = rememberBottomSheetScaffoldState(
-        //Maybe change from remember
         bottomSheetState = rememberBottomSheetState(
             initialValue = BottomSheetValue.Collapsed
         )
     )
+
+    var peekHeight: Int by rememberSaveable {
+        mutableStateOf(initialPeekHeight)
+    }
+
     val coroutineScope = rememberCoroutineScope()
 
     BottomSheetScaffold(
         scaffoldState = sheetState,
         sheetContent = {
-        //TODO: Skitcontent
-            LazyColumn(content = )
-        },
-        sheetPeekHeight = 0.dp
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(800.dp)
+            ) {
+                Text(text = "Hello from sheet")
+                Button(onClick = {
+                    coroutineScope.launch {
+                        if (sheetState.bottomSheetState.isCollapsed) {
+                            sheetState.bottomSheetState.expand()
+                        }else{
+                            sheetState.bottomSheetState.collapse()
+                        }
+                    }
+                }) {
+                    Text(text = "Up")
+                }
+                //TODO Sheet content
+            }
+        }, sheetPeekHeight = peekHeight.dp
 
     ) {
-        //TODO: Add reference to kart
+        Button(onClick = {
+
+            coroutineScope.launch {
+
+                if (sheetState.bottomSheetState.isCollapsed) {
+                    if (peekHeight == selectedPeekHeight) {
+                        peekHeight = initialPeekHeight
+                    } else {
+                        peekHeight = selectedPeekHeight
+                    }
+                } else {
+                    peekHeight = initialPeekHeight
+                    sheetState.bottomSheetState.collapse()
+                }
+            }
+        }) {
+            Text(text = "Expand/Collapse Bottom Sheet")
+        }
+        //TODO: Add reference to content
     }
 }
 
