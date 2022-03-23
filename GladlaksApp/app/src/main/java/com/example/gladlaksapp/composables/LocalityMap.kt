191f5b42dc061919b76bb78807dd21773f9231dd
martinmjensen <mamjense@ulrik.uio.no>
2022-03-23 16:21:25 +0100

    Refactored scaling of markers test

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalityMap.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalityMap.kt
@@ -1,5 +1,6 @@
 package com.example.gladlaksapp.composables
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.gladlaksapp.R
 import com.example.gladlaksapp.models.Locality
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -22,35 +24,50 @@ fun LocalityMap(
     startLng: Double = 10.7,
     onMarkerClick: (Locality) -> Boolean,
 ) {
-    var size by remember { mutableStateOf(30) }
+    val initMarkerSize = 50
+    var markerSize by remember { mutableStateOf(initMarkerSize)}
 
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(LatLng(startLat, startLng), 6f)
     }
 
-    LaunchedEffect(cameraPositionState.position.zoom) {
-        size += 10
+    LaunchedEffect(
+        cameraPositionState.position.zoom,
+        markerSize
+    ) {
+        if (cameraPositionState.position.zoom > 10f && markerSize == initMarkerSize) {
+            markerSize = initMarkerSize + 50
+        } else if (cameraPositionState.position.zoom < 10f && markerSize != initMarkerSize) {
+            markerSize = initMarkerSize
+        }
     }
-
-    val context = LocalContext.current
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
         if (localities != null) {
-            val bitmapFactory = BitmapFactory.decodeResource(context.resources, R.drawable.white_border_icon)
-            val bitmap =  Bitmap.createScaledBitmap(bitmapFactory, size, size, false)
+            val icon = createMarkerIcon(LocalContext.current, markerSize)
 
             for (loc in localities) {
                 if (!loc.isOnLand) {
                     Marker(
                         onClick = { onMarkerClick(loc) },
                         position = LatLng(loc.lat, loc.lon),
-                        icon = BitmapDescriptorFactory.fromBitmap((bitmap))
+                        icon = icon
                     )
                 }
             }
         }
     }
+}
+
+fun createMarkerIcon(context: Context, size: Int): BitmapDescriptor {
+    val bitmapIcon = Bitmap.createScaledBitmap(
+        BitmapFactory.decodeResource(context.resources, R.drawable.white_border_icon),
+        size,
+        size,
+        false
+    )
+    return BitmapDescriptorFactory.fromBitmap(bitmapIcon)
 }
