7424aa13a6d52c9a902feebb05affd73d7986b2d
martinmjensen <mamjense@ulrik.uio.no>
2022-03-27 15:53:33 +0200

    Added Sora font to theme as default

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Type.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/ui/theme/Type.kt
@@ -1,28 +1,21 @@
 package com.example.gladlaksapp.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.gladlaksapp.R
 
-// Set of Material typography styles to start with
+val SoraFont = FontFamily(
+    fonts = listOf(
+        Font(resId = R.font.sora_light, weight = FontWeight.Light),
+        Font(resId = R.font.sora_regular, weight = FontWeight.Normal),
+        Font(resId = R.font.sora_medium, weight = FontWeight.Medium),
+        Font(resId = R.font.sora_semibold, weight = FontWeight.SemiBold),
+    )
+)
+
+// Material typography styles
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = SoraFont,
 )
