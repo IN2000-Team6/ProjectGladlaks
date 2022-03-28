87f4bc06d4fc892fb45db16a3291b9dd75ec1d77
Jesperdn <jesperdn@ulrik.uio.no>
2022-03-28 17:51:20 +0200

    Add colors, start Type. Set navbar color

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySheetTop.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySheetTop.kt
@@ -6,11 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetState
-import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +27,6 @@ fun LocalitySheetTop(
             //contentAlignment = Alignment.TopStart
         ) {
             LocalitySnippet(locality = locality, sheetState, coroutineScope)
-            //Text("Lokasjon: ${locality.name}")
         }
     }
 }
--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySnippet.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySnippet.kt
@@ -4,9 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -41,11 +46,11 @@ fun LocalitySnippet(
             ) {
                 Text(
                     text = locality.name,
-                    style = MaterialTheme.typography.h6
+                    style = MaterialTheme.typography.titleMedium
                 )
                 Text(
                     text = "Lokalitet: ${locality.localityNo}",
-                    style = MaterialTheme.typography.subtitle1,
+                    style = MaterialTheme.typography.labelLarge,
                     modifier = Modifier.padding(start = 1.dp)
                 )
 
@@ -64,10 +69,17 @@ fun LocalitySnippet(
                 modifier = Modifier
                     .padding(end = 16.dp)
                     .size(width = 150.dp, height = 40.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF9CDCDA), //MaterialTheme.colorScheme.secondaryContainer,
+                    contentColor = Color(0xFF303631),//MaterialTheme.colorScheme.onSecondaryContainer,
+                    disabledContainerColor = Color(0x1F1F1F1F),
+                    disabledContentColor = Color(0xFF191C1D)
+                )
 
             ) {
                 Text(
-                    text = if (sheetState.isCollapsed) "Se mer" else "Se mindre"
+                    text = if (sheetState.isCollapsed) "Se mer" else "Se mindre",
+                    style = MaterialTheme.typography.labelLarge
                 )
             }
         }
