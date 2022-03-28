29c4034241ed5b29c8de57a673178722316fbc79
Jesperdn <jesperdn@ulrik.uio.no>
2022-03-28 10:12:50 +0200

    Update BarentswatchRepository.kt

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/datasources/BarentswatchRepository.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/datasources/BarentswatchRepository.kt
@@ -5,18 +5,37 @@ import com.example.gladlaksapp.models.Locality
 import com.example.gladlaksapp.models.LocalityDetailsWrapper
 
 object BarentswatchRepository {
-    val datasource = BarentswatchNetworkDataSource
 
+    private val datasource = BarentswatchNetworkDataSource
+
+    /**
+     * Get all localities from BarentsWatch
+     * @param year the year
+     * @param week the calendar week
+     * @return LocalitiesWrapper
+     */
     suspend fun getLocalities(year: Int, week: Int) : LocalitiesWrapper {
         return datasource.getLocalities(year,week)
     }
-    
+
+    /**
+     * Get all localities that are in water
+     * @param year the year
+     * @param week the calendar week
+     * @return a list containing all localities in sea
+     */
     suspend fun getLocalitiesInWater(year: Int, week: Int) : List<Locality> {
         return datasource.getLocalities(year, week).localities.filter {
             !it.isOnLand
         }
     }
 
+    /**
+     * Get detailed locality information for one locality. Use this for showing data in sheet
+     * @param localityNo the locality number provided by BarentsWatch
+     * @param year the year
+     * @param week the calendar week
+     */
     suspend fun getDetailedLocalityInfo(localityNo: Int, year: Int, week: Int) : LocalityDetailsWrapper {
         return datasource.getDetailedLocalityInfo(localityNo, year, week)
     }
