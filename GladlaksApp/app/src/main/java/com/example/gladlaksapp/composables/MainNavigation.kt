ad37550e73b0582e57f7c5d3baf1d955e2c13c5a
Jesperdn <jesperdn@ulrik.uio.no>
2022-03-29 15:07:40 +0200

    Update nav icons and colors

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/MainNavigation.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/MainNavigation.kt
@@ -19,20 +19,20 @@ fun MainNavigation(
     selectedItemIndex: Int,
 ) {
     val items = listOf(
-        NavItem(R.string.locality, Icons.Filled.MyLocation),
+        NavItem(R.string.locality, Icons.Filled.Place),
         NavItem(R.string.favorites, Icons.Filled.Favorite),
         NavItem(R.string.favorites, Icons.Filled.Search),
         NavItem(R.string.favorites, Icons.Filled.Settings),
     )
 
-    BottomNavigation (backgroundColor = Color(0xFFFBFDFD)) {
+    BottomNavigation (backgroundColor = Color(0xFFe9f2f1)) {
         items.forEachIndexed { index, item ->
             val label = stringResource(item.labelId)
             BottomNavigationItem(
                 selected = selectedItemIndex == index,
                 onClick = onClick,
                 label = { Text(label) },
-                icon = { Icon(item.icon, contentDescription = label) }
+                icon = { Icon(item.icon, contentDescription = label, tint = Color(0xFF01809C))}
             )
         }
     }
