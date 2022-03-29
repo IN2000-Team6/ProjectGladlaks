f1e985feb01f8e04b733fa87e71af8c184c3f7bb
martinmjensen <mamjense@ulrik.uio.no>
2022-03-29 13:47:15 +0200

    Removed sample data

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/models/Graph.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/models/Graph.kt
@@ -15,33 +15,3 @@ data class GraphLine(
     val coords: List<GraphCoords>,
     val lineWidth: Float = 3f,
 )
-
-val line1 = GraphLine(
-    coords = listOf(
-        GraphCoords(0f, 10f),
-        GraphCoords(1f, 4f),
-        GraphCoords(2f, 6f),
-        GraphCoords(3f, 3f),
-        GraphCoords(4f, 9f),
-        GraphCoords(5f, 7f),
-        GraphCoords(6f, 8f),
-        GraphCoords(8f, 6f),
-    ),
-    label = "3 meter",
-    color = Teal200,
-)
-
-val line2 = GraphLine(
-    coords = listOf(
-        GraphCoords(0f, 7f),
-        GraphCoords(1f, 8f),
-        GraphCoords(2f, 4f),
-        GraphCoords(3f, 5f),
-        GraphCoords(4f, 6f),
-        GraphCoords(5f, 6f),
-        GraphCoords(6f, 8f),
-        GraphCoords(8f, 3f),
-    ),
-    label = "10 meter",
-    color = Purple500,
-)
