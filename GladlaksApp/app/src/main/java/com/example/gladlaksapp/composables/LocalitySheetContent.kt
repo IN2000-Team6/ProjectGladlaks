4dd337ca94650c154a05596b87b315e65cf66902
martinmjensen <mamjense@ulrik.uio.no>
2022-03-29 14:28:55 +0200

    Clean up

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySheetContent.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/LocalitySheetContent.kt
@@ -1,9 +1,7 @@
 package com.example.gladlaksapp.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -12,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.example.gladlaksapp.models.LocalityDetailsWrapper
 
 @Composable
--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/ToggleArrowButton.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/ToggleArrowButton.kt
@@ -1,17 +1,14 @@
 package com.example.gladlaksapp.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.gladlaksapp.R
