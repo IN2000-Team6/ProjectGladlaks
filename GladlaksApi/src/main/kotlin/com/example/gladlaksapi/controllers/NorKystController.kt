f8f64530192a96dc094b45be40e98fc93e2556b4
martinmjensen <mamjense@ulrik.uio.no>
2022-04-04 13:03:51 +0200

    Fixed date time bug in api

--- GladlaksApi/src/main/kotlin/com/example/gladlaksapi/controllers/NorKystController.kt
+++ GladlaksApi/src/main/kotlin/com/example/gladlaksapi/controllers/NorKystController.kt
@@ -3,7 +3,7 @@ package com.example.gladlaksapi.controllers
 import com.example.gladlaksapi.models.LocalityTemperature
 import com.example.gladlaksapi.models.NorKystMaxDepth
 import com.example.gladlaksapi.repositories.fetchFromNorKyst
-import com.example.gladlaksapi.utils.getDateNowByFormat
+import com.example.gladlaksapi.utils.getDateNowByFormatWithSwitch
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -21,7 +21,7 @@ class TemperatureController {
         @RequestParam(required = true) lon: Double,
         @RequestParam(defaultValue = "1") @Min(0) @Max(NorKystMaxDepth) depth: Int,
     ): LocalityTemperature {
-        val now = getDateNowByFormat("yyyyMMdd00")
+        val now = getDateNowByFormatWithSwitch("yyyyMMdd00", 19)
         val url = "https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.fc.${now}.nc"
 
         return fetchFromNorKyst(url, lat, lon, depth)
--- GladlaksApi/src/main/kotlin/com/example/gladlaksapi/utils/Dates.kt
+++ GladlaksApi/src/main/kotlin/com/example/gladlaksapi/utils/Dates.kt
@@ -1,10 +1,22 @@
 package com.example.gladlaksapi.utils
 
-import java.time.LocalDateTime
-import java.time.ZoneOffset
+import java.time.OffsetDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-fun getDateNowByFormat(pattern: String) = LocalDateTime
-    .now(ZoneOffset.UTC)
-    .format(DateTimeFormatter.ofPattern(pattern))
-    .toString()
+/**
+ * Get the date now with an offset of a whole day based on a switch integer
+ * Example, if the switch is 12 (o'clock), and the date-time is 2022.04.04:14.00, then
+ * the date returned will be 2022.04.04. But if the date-time is 2022.04.04:10.00, then
+ * the date returned will be 2022.04.03 –– that is one day-of-month less.
+ */
+fun getDateNowByFormatWithSwitch(pattern: String, switch: Int): String {
+    val now = OffsetDateTime.now(ZoneId.of("Europe/Berlin"))
+
+    // Offset
+    val offset = if (now.hour < switch) now.withDayOfMonth(now.dayOfMonth - 1) else now
+
+    return offset
+        .format(DateTimeFormatter.ofPattern(pattern))
+        .toString()
+}
