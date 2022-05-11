4ee28bab5b12ac17a7373938267ab8c89004c26b
martinmjensen <mamjense@ulrik.uio.no>
2022-05-11 20:26:06 +0200

    Clean up

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/screens/FavoritesScreen.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/screens/FavoritesScreen.kt
@@ -3,15 +3,9 @@ package com.example.gladlaksapp.composables.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.gladlaksapp.composables.FavoritesBottomSheet
-import com.example.gladlaksapp.models.Locality
 import com.example.gladlaksapp.viewmodels.LoadedFavoriteViewModel
-import com.example.gladlaksapp.viewmodels.MainViewModel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 @Composable
 fun FavoritesScreen(
