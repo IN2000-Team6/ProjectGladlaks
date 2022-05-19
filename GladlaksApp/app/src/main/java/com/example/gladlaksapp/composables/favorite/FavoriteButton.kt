794e2d19524f3cff6e0ea3c6a8f769d23f1ddb86
martinmjensen <mamjense@ulrik.uio.no>
2022-05-19 09:55:25 +0200

    Removed outcommented code in fav button

--- GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/favorite/FavoriteButton.kt
+++ GladlaksApp/app/src/main/java/com/example/gladlaksapp/composables/favorite/FavoriteButton.kt
@@ -1,6 +1,5 @@
-package com.example.gladlaksapp.composables
+package com.example.gladlaksapp.composables.favorite
 
-import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
@@ -8,15 +7,9 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.gladlaksapp.models.Locality
-import com.example.gladlaksapp.models.FavoriteLocality
-import com.example.gladlaksapp.viewmodels.FavoriteViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 fun FavoriteButton(
@@ -36,30 +29,3 @@ fun FavoriteButton(
         )
     }
 }
-
-//TODO rewrite favorite status
-//val coroutineScope = rememberCoroutineScope()
-/*
-    val favorites by favViewModel.favorites.observeAsState()
-    //    val favorites = favViewModel.favorites
-
-    val isFavorite = favorites?.filter { it.localityNo == locality.localityNo }
-    val len = favorites?.size
-
-    LaunchedEffect(key1 = null) {
-        favViewModel.loadFavorites()
-    }
-
-
-
-    fun toggleFavorite(){
-        coroutineScope.launch {
-            favViewModel.toggleFavorite(locality)
-            Log.d("Check bool", "$isFavorite, ($len)")
-            Log.d("Selected loc", "${locality.localityNo}")
-            Log.d("loc in favs", "${favorites?.filter{it.localityNo == locality.localityNo}}")
-            Log.d("Check bool", "After: $isFavorite, ($len)")
-        }
-    }
-
- */
